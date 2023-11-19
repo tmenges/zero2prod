f5c2124f57a868871d89fb8286a800500ab17c95
Tony Menges <tony.menges@gmail.com>
2023-11-18 17:55:27 -0800

    Refactored to allow integration test to use random port

--- tests/health_check.rs
+++ tests/health_check.rs
@@ -1,24 +1,30 @@
+use std::net::TcpListener;
+use zero2prod::run;
+
+fn spawn_app() -> String {
+    let listener = TcpListener::bind("127.0.0.1:0").expect("Failed to bind random port");
+    let port = listener.local_addr().unwrap().port();
+    let server = run(listener).expect("Failed to bind address");
+    let _ = tokio::spawn(server);
+    // We return the application address to the caller!
+    format!("http://127.0.0.1:{}", port)
+}
+
 #[tokio::test]
 async fn health_check_works() {
     // Arrange
-    spawn_app();
-
-    // Need to bring in 'reqwest'
+    let address = spawn_app();
     let client = reqwest::Client::new();
 
     // Act
     let response = client
-        .get("http://127.0.0.1:8000/health_check")
+        // Use the returned application address
+        .get(&format!("{}/health_check", &address))
         .send()
         .await
         .expect("Failed to execute request.");
 
-    // Assertions
+    // Assert
     assert!(response.status().is_success());
     assert_eq!(Some(0), response.content_length());
-}
-
-fn spawn_app() {
-    let server = zero2prod::run().expect("Failed to bind address");
-    let _ = tokio::spawn(server);
 }
