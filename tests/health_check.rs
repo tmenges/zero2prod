6d4f6893074b4c413c4522f0d4084b2417a43868
Tony Menges <tony.menges@gmail.com>
2023-11-28 16:54:50 -0800

    Restructured the project for future growth

--- tests/health_check.rs
+++ tests/health_check.rs
@@ -1,5 +1,5 @@
 use std::net::TcpListener;
-use zero2prod::run;
+use zero2prod::startup::run;
 
 fn spawn_app() -> String {
     let listener = TcpListener::bind("127.0.0.1:0").expect("Failed to bind random port");
