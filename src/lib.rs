ebf00885e4834de9dc58a93a334a3ed3d398db22
Tony Menges <tony.menges@gmail.com>
2023-11-28 16:40:56 -0800

    Removed greetings code

--- src/lib.rs
+++ src/lib.rs
@@ -1,11 +1,6 @@
 use actix_web::dev::Server;
-use actix_web::{web, App, HttpRequest, HttpResponse, HttpServer, Responder};
+use actix_web::{web, App, HttpResponse, HttpServer};
 use std::net::TcpListener;
-
-async fn greet(req: HttpRequest) -> impl Responder {
-    let name = req.match_info().get("name").unwrap_or("World");
-    format!("Hello {}!", &name)
-}
 
 async fn health_check() -> HttpResponse {
     HttpResponse::Ok().finish()
@@ -15,8 +10,6 @@ pub fn run(listener: TcpListener) -> Result<Server, std::io::Error> {
     let server = HttpServer::new(|| {
         App::new()
             .route("health_check", web::get().to(health_check))
-            .route("/", web::get().to(greet))
-            .route("/{name}", web::get().to(greet))
     })
     .listen(listener)?
     .run();
